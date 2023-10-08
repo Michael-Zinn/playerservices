67be0ed8f5fa697791a29c28895f503f2c24b8cf
Christian Harnisch <chr.harnisch@gmx.net>
2023-10-08 02:00:01 +0200

    Update dependencies and Gradle wrapper

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("jvm") version "1.9.0"
-    id("io.papermc.paperweight.userdev") version "1.5.6" // the latest version can be found on the Gradle Plugin Portal
+    id("io.papermc.paperweight.userdev") version "1.5.7" // the latest version can be found on the Gradle Plugin Portal
     id("com.github.johnrengelman.shadow") version "8.1.1"
     id("com.github.ben-manes.versions") version "0.48.0"
 }
